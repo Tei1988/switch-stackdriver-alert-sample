32e106e6cfa1e72ed0bfb9d490c3f441ef28dd93
Tei1988 <tei1988@grifola.xyz>
2019-01-18 11:32:20 +0900

    correct script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -ex
 
 DOCKER_IMAGE=tei1988/cloud-sdk-alpha:229.0.0-alpine
-DOCKER_OPTIONS="\
-  -v '/usr/share/zoneinfo/Asia/Tokyo:/etc/localtime:ro'\
-  -v '${PWD}/crontabs:/var/spool/cron/crontabs:ro'\
-  -v '${PWD}/scripts:/opt/bin:ro'\
-  -w '/opt'\
+DOCKER_OPTIONS=\
+  -v /usr/share/zoneinfo/Asia/Tokyo:/etc/localtime:ro\
+  -v ${PWD}/crontabs:/var/spool/cron/crontabs:ro\
+  -v ${PWD}/scripts:/opt/bin:ro\
+  -w /opt\
   --entrypoint busybox\
   --restart always\
-  -d"
+  -d
 docker run ${DOCKER_OPTIONS} ${DOCKER_IMAGE} crond -d 8 -f
